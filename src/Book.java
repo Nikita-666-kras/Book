a5f9cf641e1ed2bd73ba86b5aae7bf12e434b96a
fminsgors <115460172+fminsgors@users.noreply.github.com>
2023-09-13 11:32:28 +0300

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Book {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите имя автора:");
+        String name_author = scanner.nextLine();
+
+        System.out.println("Укажите Название книги:");
+        String name_book = scanner.nextLine();
+
+        System.out.println("Укажите год издания книги:");
+        String date_publication = scanner.nextLine();
+
+
+
+
+
+
+
+
+    }
+}
